38bf36fd0646948f720bc6df927396f0a516211c
givehug <vboretskyi@gmail.com>
2018-05-04 13:46:13 +0300

    SUIT-9119 Extend config fields with rc file

--- config/index.js
+++ config/index.js
@@ -1,6 +1,11 @@
+const {pick} = require('ramda');
 const logLevels = require('../lib/constants/logLevels');
+const {readRcConfig} = require('../lib/utils/testLauncherHelper');
+const {validate, validators} = require('../lib/validataion');
+const {invalidConfigObj} = require('../lib/texts');
 
 const sentryDsn = 'https://1f74b885d0c44549b57f307733d60351:dd736ff3ac994104ab6635da53d9be2e@sentry.io/288812';
+const overridableFields = ['logLevel', 'useSentry'];
 
 const main = {
 	apiUrl: 'https://the.suite.st/api/public/v2',
@@ -27,9 +32,16 @@ const config = {...(global._suitestTesting ? test : main)};
  * Override config object
  * @param {Object} overrideObj
  */
-function override(overrideObj) {
-	Object.assign(config, overrideObj);
+function override(overrideObj = {}) {
+	const _overrideObj = pick(overridableFields, overrideObj);
+
+	validate(validators.CONFIGURE, _overrideObj, invalidConfigObj());
+
+	Object.assign(config, _overrideObj);
 }
+
+// override lib config with user config from .suitestrc
+override(readRcConfig());
 
 module.exports = {
 	config,
