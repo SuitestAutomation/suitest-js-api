042293b165250a3fd639c4ab6b32df1a66b09d5a
givehug <vboretskyi@gmail.com>
2018-05-10 16:39:36 +0300

    SUIT-9186 Fix sentry reporting, isSuitestUncaughtError method

--- test/utils/isSuitestUncaughtError.test.js
+++ test/utils/isSuitestUncaughtError.test.js
@@ -1,11 +1,12 @@
 const assert = require('assert');
 const isSuitestUncaughtError = require('../../lib/utils/sentry/isSuitestUncaughtError');
+const {API_CONSTRUCTOR_NAME, API_LIB_PATH_IDENTIFIERS} = require('../../lib/constants');
 
 it('should check non SuitestError with no footprints in stacktrace', () => {
 	assert.strictEqual(isSuitestUncaughtError({
 		type: 'TypeError',
 		stacktrace: {
-			frames: [{function: 'someFunctionName'}],
+			frames: [{filename: 'someFunctionName'}],
 		},
 	}), false, 'false');
 });
@@ -14,7 +15,13 @@ it('should check non SuitestError with some footprints in stacktrace', () => {
 	assert.strictEqual(isSuitestUncaughtError({
 		type: 'TypeError',
 		stacktrace: {
-			frames: [{function: 'SUITEST_API.openSession'}],
+			frames: [{filename: API_CONSTRUCTOR_NAME + '.openSession'}],
+		},
+	}), true, 'true');
+	assert.strictEqual(isSuitestUncaughtError({
+		type: 'TypeError',
+		stacktrace: {
+			frames: [{filename: API_LIB_PATH_IDENTIFIERS[2]}],
 		},
 	}), true, 'true');
 });
