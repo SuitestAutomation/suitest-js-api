a11f515caeb40d40a0eba5b2c077f935bcc59691
givehug <vboretskyi@gmail.com>
2018-08-23 13:04:57 +0300

    Improve logger, repl UT

--- test/testLauncher/repl.test.js
+++ test/testLauncher/repl.test.js
@@ -1,21 +1,27 @@
 const assert = require('assert');
 const repl = require('repl');
 const sinon = require('sinon');
+const EventEmitter = require('events');
+
+// mock repl instance
+class ReplInstance extends EventEmitter {
+	constructor() {
+		super();
+		this.context = {};
+		setTimeout(() => this.emit('exit'), 50);
+		this.close = () => void 0;
+	}
+}
 
 describe('repl', () => {
 	before(() => {
-		sinon.stub(repl, 'start').returns({
-			close: () => void 0,
-			context: {},
-			on: () => void 0,
-		});
+		sinon.stub(repl, 'start').returns(new ReplInstance());
 		require('../../lib/utils/testHelpers/mocks').restoreRepl();
 	});
 
 	after(() => {
 		repl.start.restore();
 		require('../../lib/utils/testHelpers/mocks').stubRepl();
-		require('../../lib/testLauncher/repl').setActive(false);
 	});
 
 	it('should test startRepl', async() => {
@@ -26,5 +32,7 @@ describe('repl', () => {
 		assert.strictEqual(isActive(), true, 'repl is active');
 		assert.strictEqual(repl.start.called, true, 'repl start called');
 		assert.strictEqual(promsie instanceof Promise, true, 'promise returned');
+		await promsie;
+		assert.strictEqual(isActive(), false, 'repl is back to inactive');
 	});
 });
