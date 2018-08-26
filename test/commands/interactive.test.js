b3a6568f3acd28bb2dda0167e0f7a45a359a2ed2
Vova <vboretskyi@gmail.com>
2018-08-26 15:31:30 +0300

    SUIT-10382 Update REPL

--- test/commands/interactive.test.js
+++ test/commands/interactive.test.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const sinon = require('sinon');
+const path = require('path');
 
 const {config, extend} = require('../../config');
 const logger = require('../../lib/utils/logger');
@@ -48,6 +49,32 @@ describe('interactive command', () => {
 
 		assert.strictEqual(result, undefined, 'promise resolved');
 		assert.strictEqual(startRepl.called, true, 'repl called');
+		assert.deepEqual(startRepl.lastCall.args[0].dir, __dirname, 'dir');
+		assert.deepEqual(startRepl.lastCall.args[0].require, [], 'require');
+		assert.strictEqual(warnStub.called, false, 'warning not displayed');
+	});
+
+	it('should start repl with require files', async() => {
+		const repl = config.repl;
+
+		extend({repl: true});
+		let result = true;
+
+		sinon.stub(logger, 'info');
+		const warnStub = sinon.stub(logger, 'warn');
+
+		try {
+			result = await interactive({require: ['test.js']});
+		} finally {
+			logger.info.restore();
+			warnStub.restore();
+			extend({repl});
+		}
+
+		assert.strictEqual(result, undefined, 'promise resolved');
+		assert.strictEqual(startRepl.called, true, 'repl called');
+		assert.deepEqual(startRepl.lastCall.args[0].dir, __dirname, 'dir');
+		assert.deepEqual(startRepl.lastCall.args[0].require, [path.resolve(__dirname, 'test.js')], 'require');
 		assert.strictEqual(warnStub.called, false, 'warning not displayed');
 	});
 });
--- test/testLauncher/repl.test.js
+++ test/testLauncher/repl.test.js
@@ -3,13 +3,19 @@ const repl = require('repl');
 const sinon = require('sinon');
 const EventEmitter = require('events');
 
+let context;
+let evalStub;
+let defineCommandStub;
+
 // mock repl instance
 class ReplInstance extends EventEmitter {
 	constructor() {
 		super();
-		this.context = {};
+		this.context = context = {};
 		setTimeout(() => this.emit('exit'), 50);
-		this.close = () => void 0;
+		this.close = sinon.stub();
+		this.eval = evalStub = sinon.stub();
+		this.defineCommand = defineCommandStub = sinon.stub();
 	}
 }
 
@@ -26,11 +32,14 @@ describe('repl', () => {
 
 	it('should test startRepl', async() => {
 		const {startRepl, isActive} = require('../../lib/testLauncher/repl');
-		const suitestInstance = {};
-		const promsie = startRepl(suitestInstance);
+		const testContext = {suitest: 'suitest'};
+		const promsie = startRepl({context: testContext});
 
 		assert.strictEqual(isActive(), true, 'repl is active');
 		assert.strictEqual(repl.start.called, true, 'repl start called');
+		assert.strictEqual(evalStub.called, true, 'replInstance eval called');
+		assert.strictEqual(defineCommandStub.lastCall.args[0], 'r', '.r command defined');
+		assert.deepEqual(context, testContext, 'replInstance context extended');
 		assert.strictEqual(promsie instanceof Promise, true, 'promise returned');
 		await promsie;
 		assert.strictEqual(isActive(), false, 'repl is back to inactive');
