c0cc6697a9ff9b122a989b9f9a0eb64430023ae0
anton <anton.b@mineus.net>
2018-09-05 11:54:04 +0300

    SUIT-10511 Add new Roku properties

--- testDefinition/examplesTS/elementChain.ts
+++ testDefinition/examplesTS/elementChain.ts
@@ -1,4 +1,4 @@
-import { element } from '../../index';
+import { element, assert, PROP } from '../../index';
 
 const el = element({
     css: '.my-element',
@@ -9,7 +9,27 @@ el.moveTo();
 el.sendText('');
 el.timeout(1);
 el.exists();
+el.matches(PROP.IS_CHECKED, true);
 el.then(e => e.backgroundColor);
 el.then(e => e.text);
 el.then(e => e.id);
 
+// roku
+el.matches([
+	{
+		name: PROP.FONT_NAME,
+		val: 'sdf',
+	},
+	{
+		name: PROP.FONT_SIZE,
+		val: 123,
+	},
+	{
+		name: PROP.ITEM_FOCUSED,
+		val: 11,
+	},
+	{
+		name: PROP.IS_CHECKED,
+		val: true,
+	},
+]);
