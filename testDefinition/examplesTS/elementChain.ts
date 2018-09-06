fa6e4fb046a7d6a9a7e90171ea30010ea6cc951b
AntonSBilan <AntonSBilan@gmail.com>
2018-09-06 12:41:09 +0100

    SUIT-10511 Add new Roku properties (#127)
    * SUIT-10511
    Add new Roku properties
    
    * fontName => fontURI
    
    * visible => visibility

--- testDefinition/examplesTS/elementChain.ts
+++ testDefinition/examplesTS/elementChain.ts
@@ -1,4 +1,4 @@
-import { element } from '../../index';
+import { element, assert, PROP, VISIBILITY_STATE } from '../../index';
 
 const el = element({
     css: '.my-element',
@@ -9,7 +9,27 @@ el.moveTo();
 el.sendText('');
 el.timeout(1);
 el.exists();
+el.matches(PROP.IS_CHECKED, true);
 el.then(e => e.backgroundColor);
 el.then(e => e.text);
 el.then(e => e.id);
 
+// roku
+el.matches([
+	{
+		name: PROP.FONT_NAME,
+		val: 'string',
+	},
+	{
+		name: PROP.VISIBILITY,
+		val: VISIBILITY_STATE.VISIBLE,
+	},
+	{
+		name: PROP.ITEM_FOCUSED,
+		val: 1,
+	},
+	{
+		name: PROP.FONT_URI,
+		val: 'string',
+	},
+]);
