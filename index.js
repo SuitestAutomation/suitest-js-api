4060bf3bfa439b1b4cd16fd4083617cd055abbf3
Vladimir Boretskyi <givehug@users.noreply.github.com>
2018-09-19 13:14:53 +0300

    SUIT-10282 Add windows process shutdown workaround (#126)

--- index.js
+++ index.js
@@ -133,6 +133,16 @@ envHelper.handleUserEnvVar();
 // Export public API
 module.exports = new SUITEST_API();
 
+// Old Windows/Node process shutdown workaround
+let winRl;
+
+if (process.platform === 'win32') {
+	winRl = require('readline').createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+}
+
 // Listen to process events to trigger websocket termination and dump warnings, if any
 const shutDown = () => {
 	// Make sure socket connection is done
@@ -164,7 +174,12 @@ const shutDown = () => {
 
 	// Ctrl+Break on Windows
 	'SIGBREAK',
-].forEach(term => process.on(term, shutDown));
+].forEach(term => {
+	if (winRl && term !== 'exit')
+		winRl.on(term, () => process.emit(term));
+
+	process.on(term, shutDown);
+});
 
 // Exit process with code 1 on uncaughtException or unhandledRejection
 // Required for proper termination of test launcher child processes
