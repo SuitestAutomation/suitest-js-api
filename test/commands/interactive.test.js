7b99745aacb3be642349af100e8295ce310d2cec
Taras Perevorskyy <taras@mineus.net>
2018-10-03 17:52:49 +0200

    fixed failing test

--- test/commands/interactive.test.js
+++ test/commands/interactive.test.js
@@ -70,7 +70,7 @@ describe('interactive command', () => {
 		const replWelcomeMessage = sinon.spy(texts, 'replWelcomeMessage');
 
 		// eslint-disable-next-line max-len
-		const expected = 'Test execution has been paused for the interactive session\nNow you can:\n\n  1. Edit watched files - Suitest will reload them and execute the repeater\n     function every time they change on disk.\n  2. Use the prompt below to execute any JavaScript in real time.\n\nHere is your environment:\n\n  Current working dir: \u001b[37mC:\\Users\\Taras\\projects\\suitest-js-api\\test\\commands\u001b[39m\n  Repeater function: \u001b[37mnone\u001b[39m\n  Available local variables: \u001b[37msuitest\u001b[39m\n  Watched files (relative to your working dir):\n    - \u001b[37mC:\\Users\\Taras\\projects\\suitest-js-api\\test\\commands\\**\\*.js\u001b[39m\n\n';
+		const expected = 'Test execution has been paused for the interactive session\nNow you can:\n\n  1. Edit watched files - Suitest will reload them and execute the repeater\n     function every time they change on disk.\n  2. Use the prompt below to execute any JavaScript in real time.\n\nHere is your environment:\n\n  Current working dir: \u001b[37m' + __dirname + '\u001b[39m\n  Repeater function: \u001b[37mnone\u001b[39m\n  Available local variables: \u001b[37msuitest\u001b[39m\n  Watched files (relative to your working dir):\n    - \u001b[37m'+ path.join(__dirname, '\\**\\*.js') + '\u001b[39m\n\n';
 
 		await interactive();
 
