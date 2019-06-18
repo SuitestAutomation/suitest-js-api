4cb85f443bdde32aaa3bf4bb24301e96792c1369
Vladimir Boretskyi <givehug@users.noreply.github.com>
2019-06-18 12:29:00 +0300

    Release/assert for loaded image (#206)
    * SUIT-12222 Added imageLoadState (#201)
    
    * SUIT-12222 Added imageLoadState
    
    * SUIT-12222 Fix import path
    
    * Feature/suit 12222 new element property imageloa (#202)
    
    * SUIT-12222 Added imageLoadState
    
    * SUIT-12222 Fix import path
    
    * SUIT-12222 Updated EMPTY value for imageLoadState prop
    
    * Feature/suit 12222 empty string logs placeholder (#203)
    
    * SUIT-12222 Replace empty strings in logs with [EMPTY] placeholder
    
    * SUIT-12222 Add empty placeholder to element selector, props, ws messages
    
    * SUIT-12222 Replace empty string in element chain values only
    
    * Feature/suit 12222 empty string logs placeholder (#204)
    
    * SUIT-12222 Replace empty strings in logs with [EMPTY] placeholder
    
    * SUIT-12222 Add empty placeholder to element selector, props, ws messages
    
    * SUIT-12222 Replace empty string in element chain values only
    
    * SUIT-12222 Placehold empty element values in error message

--- test/utils/stringUtils.test.js
+++ test/utils/stringUtils.test.js
@@ -5,6 +5,7 @@ const {
 	arrToString,
 	stripAnsiChars,
 	appendDot,
+	placeholdEmpty,
 } = require('../../lib/utils/stringUtils');
 
 describe('stringUtils util', () => {
@@ -23,5 +24,11 @@ describe('stringUtils util', () => {
 		assert.equal(appendDot(''), '.');
 		assert.equal(appendDot('test'), 'test.');
 		assert.equal(appendDot('test.'), 'test.');
+	});
+
+	it('placeholdEmpty should transform empty strings properly', () => {
+		assert.equal(placeholdEmpty(''), '[EMPTY]');
+		assert.equal(placeholdEmpty(' '), ' ');
+		assert.equal(placeholdEmpty(1), 1);
 	});
 });
---
+++ typeDefinition/constants/ImageLoadState.d.ts
@@ -0,0 +1,7 @@
+export type ImageLoadState = {
+    LOADED: 'loaded',
+    LOADING: 'loading',
+    ERROR: 'error',
+    UNKNOWN: 'unknown',
+    EMPTY: '',
+}
