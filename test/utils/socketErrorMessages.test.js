14de673409771de477cb9793258b7cfaf84dd945
JeKachan <jekachan7@gmail.com>
2020-06-19 17:34:23 +0300

    SUIT-14146 added translation for MoveTargetOutOfBounds error.

--- test/utils/socketErrorMessages.test.js
+++ test/utils/socketErrorMessages.test.js
@@ -584,6 +584,7 @@ describe('Socket error messages', () => {
 			[basePayload('outOfMemory'), 'Failed to open the app. Device is out of memory, please restart the device.'],
 			[basePayload('configuratorError'), 'Make sure that Apple Configurator 2 and Automation Tools are installed. Please see our docs.'],
 			[basePayload('appStoreBuild'), 'Can’t install App Store distribution build.'],
+			[basePayload('MoveTargetOutOfBounds'), 'Element is outside of the visible area of the screen.'],
 		].forEach(([payload, expectMessage]) => {
 			it(expectMessage, () => {
 				assert.strictEqual(stripAnsiChars(getErrorMessage(payload)), expectMessage, JSON.stringify(payload, null, 4));
