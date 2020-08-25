c1dc6537df3c4744b6aefde8ed77f3cb8ba8c0c4
Ievgen Kozynskyi <jekachan7@gmail.com>
2020-08-25 11:42:20 +0300

    SUIT-9452 Update "failed to initialise the device" message (#297)

--- test/utils/socketErrorMessages.test.js
+++ test/utils/socketErrorMessages.test.js
@@ -98,7 +98,7 @@ describe('Socket error messages', () => {
 			[basePayload('failedStart'), 'Chain description.'],
 			[basePayload('missingApp'), 'Application is not installed on the device.'],
 			[basePayload('outdatedLibrary'), 'We have detected that your instrumentation library is outdated and the package cannot be opened. Update required.'],
-			[basePayload('initPlatformFailed'), 'Failed to start Suitest bootstrap application on this device.'],
+			[basePayload('initPlatformFailed'), 'Failed to bootstrap platform on this device.'],
 			[basePayload('packageNotFound'), 'There is nothing to test, because the selected configuration does not contain an app package. Upload a package on your app\'s configuration page before continuing.'],
 			[basePayload('internalError'), 'Internal error occurred. Chain description.'],
 			[basePayload('ILInternalError'), 'Internal error occurred. Chain description.'],
