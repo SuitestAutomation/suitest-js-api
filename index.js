a093ccb32fed02ddf2e76de62ea3238c2a046a8b
Tomáš Polomský <polomsky@users.noreply.github.com>
2023-05-04 19:30:23 +0200

    prevent silent errors

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const suitest = new SUITEST_API({exitOnError: true});
 
 // Check if we are in launcher child process, connect to master IPC,
 // override config, start session, pair device, set app config
-connectToIpcAndBootstrapSession(suitest);
+connectToIpcAndBootstrapSession(suitest).catch(function(err) {
+	suitest.logger.error(err);
+	process.exit(1);
+});
 
 // Export public API
 module.exports = suitest;
